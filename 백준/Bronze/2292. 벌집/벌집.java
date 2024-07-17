4920bc0979411acfda9672d168ba5a6db7eb8495
HyeranJo <124690890+HyeranJo@users.noreply.github.com>
2024-07-17 22:23:20 +0900

    [Bronze II] Title: 벌집, Time: 212 ms, Memory: 18996 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -4,13 +4,16 @@ public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int data = 1;
-        int level = 1;
-        // System.out.println(calc(num, 1, 1));
-        while(num > data) {
-            data += (6 * level);
-            ++level;
-        }
-        System.out.println(level);
+
+        if(num == 1) System.out.println(1);
+        else if(num <= 7) System.out.println(2);
+        else System.out.println(calc(num, 1, 0));
+    }
+    public static int calc(int num, int data, int level) {
+        if(num <= data) return 0;
+
+        if(num <= 1) return 1;
+        else if(num <= 2) return calc(num -= 1, 6, ++level) + 1;
+        else return calc(num -= data, 6 * level, ++level) + 1;
     }
 }
